1abf5b33f3f076ff1eb5626b5e30e90d34126351
Peter Z. <pzverkov@users.noreply.github.com>
2026-06-01 21:15:11 +0200

    prepare the v0.0.11 release

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -9,7 +9,7 @@ const (
 	// Minor is the minor version (new features).
 	Minor = 0
 	// Patch is the patch version (bug fixes).
-	Patch = 10
+	Patch = 11
 	// Label is the optional pre-release label.
 	Label = ""
 )
